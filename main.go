b0d2a1bb354f0863c4fd3947086cf40f938ebe34
Gonzalo Honores <ghonores@gmail.com>
2023-10-05 10:26:14 -0300

    agregue campo fecha en usuario

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "gosql:20152008Gus!@tcp(127.0.0.1:3306)/ejemplodb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic(err)
+		os.Exit(-1)
+	}
+	//fmt.Println(db.Name())
+	//fmt.Print(dsn)
+
+	type User struct {
+		ID   uint
+		Name string
+		Age  int
+		// hundreds of fields
+	}
+
+	user := User{Name: "Arturo", Age: 56}
+	result := db.Create(&user) // pass pointer of data to Create
+	//user.ID             // returns inserted data's primary key
+	//result.Error        // returns error
+	//result.RowsAffected // returns inserted records count
+
+	fmt.Print(result.RowsAffected)
+
+}
